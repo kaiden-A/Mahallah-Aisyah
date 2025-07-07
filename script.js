269ebae45c3cbef918a9bdab217b93d09c51bf1a
Amirul Ikhwan <amirikhwanfaisal@gmail.com>
2025-07-07 19:06:14 +0800

    version 2.0

--- script.js
+++ script.js
@@ -3,13 +3,16 @@
 const btn = document.getElementById('button');
 const blockCard = document.getElementById('block');
 const floorCard = document.getElementById('floor');
+const listCard = document.getElementById('list');
 
 btn.addEventListener('click' , async event => {
 
     const roomNumber = document.getElementById('room-number').value;
 
     const data = await getRoom(roomNumber);
-    displayData(data[0] , data[1]);
+    console.log(data);
+    displayRoomFloor(data.room[0] , data.room[1]);
+    displayListRoom(data.listRoom , data.room[0] , data.room[1]);
 
 
 })
@@ -19,10 +22,26 @@ btn.addEventListener('click' , async event => {
 async function getRoom(roomNumber){
 
     try{
-       const responses = await fetch('room.json'); 
 
-       const data = await responses.json();
-       return data[roomNumber];
+       const responses = await fetch('room.json'); 
+       const datas = await responses.json();
+
+       const listRoom = [];
+
+       const floor = datas[roomNumber][0];
+       const block = datas[roomNumber][1];
+
+       Object.entries(datas).forEach(([key , value]) => {
+
+            if(value[0] === floor && value[1] === block){
+                listRoom.push({room: key , data: value});
+            }
+       })
+
+       console.log(listRoom);
+
+
+       return {room : datas[roomNumber] , listRoom : listRoom};
 
     }catch(err){
 
@@ -33,7 +52,7 @@ async function getRoom(roomNumber){
     
 }
 
-function displayData(floor , block){
+function displayRoomFloor(floor , block){
 
     blockCard.innerHTML = "";
     floorCard.innerHTML = "";
@@ -50,4 +69,27 @@ function displayData(floor , block){
     blockCard.appendChild(blockNumber);
     floorCard.appendChild(floorNumber);
 
+}
+
+function displayListRoom(listRoom , floor , block){
+
+    listCard.innerHTML = "";
+    const head = document.createElement('p');
+    const list = document.createElement('p');
+
+    const listText = getList(listRoom);
+
+    head.innerText = `Rooms in Floor ${floor} Block ${block}`;
+    list.innerText = listText;
+
+    listCard.style.display = "block";
+
+    listCard.appendChild(head);
+    listCard.appendChild(list);
+
+
+}
+
+function getList(listRoom){
+    return listRoom.map(list => list.room).join(',');
 }
